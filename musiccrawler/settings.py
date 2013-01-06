ab33bbd3b4120e7d194cf37db9cc2e76bee9eb77
Thimo Brinkmann <thimo.brinkmann@googlemail.com>
2013-01-06 10:41:31 +0100

    started config
    commit in order to continue on different machine, nothing works yet

---
+++ musiccrawler/settings.py
@@ -0,0 +1,19 @@
+# Scrapy settings for musiccrawler project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'musiccrawler'
+
+SPIDER_MODULES = ['musiccrawler.spiders']
+NEWSPIDER_MODULE = 'musiccrawler.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'musiccrawler (+http://www.yourdomain.com)'
+
+MEMUSAGE_ENABLED = True
+MEMUSAGE_NOTIFY_MAIL = ['thimo.brinkmann@googlemail.com']
+MEMUSAGE_WARNING_MB = 500
