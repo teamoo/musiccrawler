ff47e788bf27e610312d624906610c37141cddb6
Thimo Brinkmann <thimo.brinkmann@googlemail.com>
2013-02-17 01:38:30 +0100

    again for eggifying...

--- musiccrawler/spiders/facebookgroupspider.py
+++ musiccrawler/spiders/facebookgroupspider.py
@@ -5,6 +5,8 @@ from scrapy.xlib.pydispatch import dispatcher
 from scrapy.spider import BaseSpider
 from datetime import datetime
 from dateutil.parser import parse
+from pkgutil import get_loader
+import os, sys
 import pymongo
 import json
 import math
@@ -30,8 +32,8 @@ class FacebookGroupSpider(BaseSpider):
         #TODO das muss noch bereinigt werden, die Gruppennummer aus dem Link holen
         self.start_urls = ["https://graph.facebook.com/137328326321645/feed?access_token=" + accesstoken]
         
-        hosts = json.load(open(musiccrawler.settings.HOSTS_FILE_PATH))
-        decrypters = json.load(open(musiccrawler.settings.DECRYPTER_FILE_PATH))
+        hosts = json.load(open(get_data_smart("spiders", "hosts.json", as_string=False)))
+        decrypters = json.load(open(get_data_smart("spiders", "decrypter.json", as_string=False)))
         regex_group_count = 40
         self.regexes = []
 
@@ -113,3 +115,26 @@ class FacebookGroupSpider(BaseSpider):
         if reason == "finished":
             print "Spider finished, updating site record"
             self.collection.update({"feedurl" : self.source},{"$set" : {"last_crawled" : datetime.now(), "next_crawl" : None}})
+
+def get_data_smart(package, resource, as_string=True):
+    """Rewrite of pkgutil.get_data() that actually lets the user determine if data should
+    be returned read into memory (aka as_string=True) or just return the file path.
+    """
+    
+    loader = get_loader(package)
+    if loader is None or not hasattr(loader, 'get_data'):
+        return None
+    mod = sys.modules.get(package) or loader.load_module(package)
+    if mod is None or not hasattr(mod, '__file__'):
+        return None
+    
+    # Modify the resource name to be compatible with the loader.get_data
+    # signature - an os.path format "filename" starting with the dirname of
+    # the package's __file__
+    parts = resource.split('/')
+    parts.insert(0, os.path.dirname(mod.__file__))
+    resource_name = os.path.join(*parts)
+    if as_string:
+        return loader.get_data(resource_name)
+    else:
+        return resource_name
--- musiccrawler/spiders/feedspider.py
+++ musiccrawler/spiders/feedspider.py
@@ -10,7 +10,12 @@ from scrapy.spider import BaseSpider
 from scrapy import log
 from scrapy import signals
 from scrapy.xlib.pydispatch import dispatcher
-
+from pkgutil import get_loader
+from datetime import datetime
+from time import mktime
+from musiccrawler.items import DownloadLinkItem
+from scrapy.http import Request
+import os, sys
 import pymongo
 import feedparser
 import re
@@ -18,10 +23,8 @@ import json
 import math
 import musiccrawler.settings
 import monthdelta
-from datetime import datetime
-from time import mktime
-from musiccrawler.items import DownloadLinkItem
-from scrapy.http import Request
+import pkgutil
+
 
 class FeedSpider(BaseSpider):        
     name = "feedspider"
@@ -43,8 +46,8 @@ class FeedSpider(BaseSpider):
             
         log.msg("Received Site from Database:" + str(self.site), level=log.INFO)
         
-        hosts = json.load(open(musiccrawler.settings.HOSTS_FILE_PATH))
-        decrypters = json.load(open(musiccrawler.settings.DECRYPTER_FILE_PATH))
+        hosts = json.load(open(get_data_smart("spiders", "hosts.json", as_string=False)))
+        decrypters = json.load(open(get_data_smart("spiders", "decrypter.json", as_string=False)))
         regex_group_count = 35
         self.regexes = []
         
@@ -145,3 +148,26 @@ class FeedSpider(BaseSpider):
     def handle_spider_closed(self, spider, reason):
         if reason == "finished":
             self.collection.update({"feedurl" : self.source},{"$set" : {"last_crawled" : datetime.now(), "next_crawl" : None}})
+            
+def get_data_smart(package, resource, as_string=True):
+    """Rewrite of pkgutil.get_data() that actually lets the user determine if data should
+    be returned read into memory (aka as_string=True) or just return the file path.
+    """
+    
+    loader = get_loader(package)
+    if loader is None or not hasattr(loader, 'get_data'):
+        return None
+    mod = sys.modules.get(package) or loader.load_module(package)
+    if mod is None or not hasattr(mod, '__file__'):
+        return None
+    
+    # Modify the resource name to be compatible with the loader.get_data
+    # signature - an os.path format "filename" starting with the dirname of
+    # the package's __file__
+    parts = resource.split('/')
+    parts.insert(0, os.path.dirname(mod.__file__))
+    resource_name = os.path.join(*parts)
+    if as_string:
+        return loader.get_data(resource_name)
+    else:
+        return resource_name
