8aa73ab2a6c1e51b9e323a84bbdbe478e8b2c908
Thimo Brinkmann <thimo.brinkmann@googlemail.com>
2013-02-23 00:22:17 +0100

    looks good

--- musiccrawler/spiders/feedspider.py
+++ musiccrawler/spiders/feedspider.py
@@ -24,6 +24,7 @@ import sys
 import pymongo
 import re
 import pkgutil
+import StringIO
 
 class FeedSpider(BaseSpider):        
     name = "feedspider"
@@ -49,9 +50,9 @@ class FeedSpider(BaseSpider):
         if self.active == False:
             log.msg("Site is deactivated, not crawling.", level=log.ERROR);
         else:
-            hosts = json.load(open(pkgutil.get_data('', musiccrawler.settings.HOSTS_FILE_PATH)))
+            hosts = json.load(open(StringIO(pkgutil.get_data('', musiccrawler.settings.HOSTS_FILE_PATH))))
             
-            decrypters = json.load(open(pkgutil.get_data(''.musiccrawler.settings.DECRYPTERS_FILE_PATH)))
+            decrypters = json.load(StringIO(open(pkgutil.get_data(''.musiccrawler.settings.DECRYPTERS_FILE_PATH))))
             regex_group_count = 35
             self.regexes = []
             
