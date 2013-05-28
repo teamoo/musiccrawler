1d4459d4a4f7142fad8fa29b767a826d6db9696b
Thimo Brinkmann <thimo.brinkmann@googlemail.com>
2013-05-28 10:53:24 +0200

    text correction

--- musiccrawler/spiders/checkunknownlinksspider.py
+++ musiccrawler/spiders/checkunknownlinksspider.py
@@ -30,14 +30,14 @@ class CheckUnknownLinksSpider(BaseSpider):
             self.db.authenticate(musiccrawler.settings.MONGODB_USER, musiccrawler.settings.MONGODB_PASSWORD)
         self.collection = self.db['links']
         self.unknownlinks = self.collection.find({"status": 'unknown'})
-        log.msg("Received " + str(self.unknownlinks.count()) +  " unknown links from Database", level=log.INFO)
+        log.msg("Received " + str(self.unknownlinks.count()) +  " UNKNOWN links from Database", level=log.INFO)
         self.oldofflinelinkes = self.collection.find({'status': 'off', 'date_published': {'$lte': (datetime.now()-timedelta(days=90))}})
-        log.msg("Removing " + str(self.oldofflinelinkes.count()) +  " from Database that are OFFLINE OR UNKNOWN and older than 90 days.", level=log.INFO)
+        log.msg("Removing " + str(self.oldofflinelinkes.count()) +  " links from Database that are OFFLINE OR UNKNOWN and older than 90 days.", level=log.INFO)
         self.collection.remove({'status': 'off', 'date_published': {'$lte': (datetime.now()-timedelta(days=90))}},False)
         self.collection.remove({'status': 'unknown', 'date_published': {'$lte': (datetime.now()-timedelta(days=90))}},False)
         
         self.oldlinks = self.collection.find({'date_published': {'$lte': (datetime.now()-timedelta(days=365))}})
-        log.msg("Removing " + str(self.oldofflinelinkes.count()) +  " from Database that are older than one year.", level=log.INFO)
+        log.msg("Removing " + str(self.oldofflinelinkes.count()) +  " links from Database that are older than one year.", level=log.INFO)
         self.collection.remove({'date_published': {'$lte': (datetime.now()-timedelta(days=365))}},False)   
                   
     def parse(self, response):
